43ec2af1a89f1c7e61962c6b02ae43414139c203
unknown <fredericreact@gmail.com>
2022-10-21 15:39:50 +0800

    map added and data dynamically rendered, with array can be updated

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,17 +23,17 @@ const [filteredYear,setFilteredYear] =useState('2020');
    onChangeFilter={filterChangeHandler}
    selected={filteredYear}
 />
-        <ExpenseItem 
-      title={props.items[0].title} 
-      amount={props.items[0].amount}
-        date={props.items[0].date}
-       
-      />
-      <ExpenseItem 
-      title={props.items[1].title} 
-      amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
+
+{props.items.map((expense)=>(
+<ExpenseItem
+key={expense.id}
+title={expense.title}
+amount = {expense.amount}
+date ={expense.date}
+/>
+))
+}
+    
       </Card>
 
       </div>
